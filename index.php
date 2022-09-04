d888d02deed02eb1dd97beb8685122a60e24adcc
nedicom <m6132@yandex.ru>
2022-09-05 00:11:03 +0300

    add bulma header, footer, main and script of menu in main

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <!-- Голова страницы сайта -->
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>тайтл</title>
+        <!-- JQ cdn -->        
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+        <!-- Bulma CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+
+          <!-- LESS  CSS JS
+          <link rel="stylesheet/less" type="text/css" href="css/style.less" />
+          <script src="https://cdn.jsdelivr.net/npm/less" ></script>
+          -->
+
+
+    </head>
+
+    <!-- Тело страницы  сайта -->
+    <body>
+        <?php include 'src/header.php';?>
+        <?php include 'src/main.php';?>
+        <?php include 'src/footer.php';?>
+      </body>
+</html>
